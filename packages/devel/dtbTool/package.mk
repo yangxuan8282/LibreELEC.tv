fb8ee4eff51ab91937700417c56c2130c9fe1f9d
Wilhansen Li <wil@byimplication.com>
2016-12-30 06:22:08 +0000

    Add dtbTool source and remove pre-built binary.
    dtbTool now builds to $ROOT/$TOOLCHAIN/bin and can be used by more
    platforms.

---
+++ packages/devel/dtbTool/package.mk
@@ -0,0 +1,17 @@
+PKG_NAME="dtbTool"
+PKG_VERSION="1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="BSD-3-Clause"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="devel"
+PKG_SHORTDESC="Amlogic DTB combiner."
+PKG_LONGDESC="Combines DTBs into Amlogic's multi-DTB format."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+makeinstall_host() {
+    mkdir -p "$ROOT/TOOLCHAIN/bin"
+    cp -a "$ROOT/$PKG_BUILD/dtbTool" "$ROOT/$TOOLCHAIN/bin/"
+}
