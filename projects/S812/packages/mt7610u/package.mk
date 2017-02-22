a57982321ee0de651d0906875a00d38604ca23ad
Oleg Ivanov <leo-sp50@yandex.ru>
2017-02-22 11:03:52 +0300

    S812 krypton 20170222

--- projects/S812/packages/mt7610u/package.mk
+++ projects/S812/packages/mt7610u/package.mk
@@ -37,15 +37,15 @@ PKG_AUTORECONF="no"
 make_target() {
 
   sed -i '198s|.*LINUX_SRC.*|LINUX_SRC = '$(kernel_path)'|' Makefile
-  sed -i '199s|.*LINUX_SRC_MODULE.*|LINUX_SRC_MODULE = '$INSTALL'/lib/modules/'$(get_module_dir)'/kernel/drivers/net/wireless/|' Makefile
+  sed -i '199s|.*LINUX_SRC_MODULE.*|LINUX_SRC_MODULE = '$INSTALL'/usr/lib/modules/'$(get_module_dir)'/kernel/drivers/net/wireless/|' Makefile
   make osdrv
 
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/lib/modules/$(get_module_dir)/$PKG_NAME
-    cp os/linux/mt7610u_sta.ko $INSTALL/lib/modules/$(get_module_dir)/$PKG_NAME
+  mkdir -p $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
+    cp os/linux/mt7610u_sta.ko $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
 
-  mkdir -p $INSTALL/lib/firmware/mt7610u
-    cp RT2870STA.dat $INSTALL/lib/firmware/mt7610u/mt7610u_sta.dat
+  mkdir -p $INSTALL/usr/lib/firmware/mt7610u
+    cp RT2870STA.dat $INSTALL/usr/lib/firmware/mt7610u/mt7610u_sta.dat
 }
