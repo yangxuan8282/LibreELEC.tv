185e3fc9dd48b582fe8978df20eedbeb440c9800
Oleg Ivanov <leo-sp50@yandex.ru>
2017-08-10 15:45:30 +0300

    v20170810

--- projects/S805/packages/initramfs/package.mk
+++ projects/S805/packages/initramfs/package.mk
@@ -57,9 +57,7 @@ post_install() {
     ln -sf /usr/bin $BUILD/initramfs/bin
     ln -sf /usr/sbin $BUILD/initramfs/sbin
 
-    rm $BUILD/initramfs/splash/splash-768.png
-    rm $BUILD/initramfs/splash/splash-1200.png
-    rm $BUILD/initramfs/splash/splash-2160.png
+    ln -sf busybox $BUILD/initramfs/usr/bin/fbset
 
     mkdir -p $BUILD/image/
     fakeroot -- sh -c \
