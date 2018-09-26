a5a3abed13a505cdc260acba8cb03ae2f7717e18
Christian Hewitt <christianshewitt@gmail.com>
2018-09-26 09:53:29 +0400

    firmware: add ath10k-firmware package

---
+++ packages/linux-firmware/ath10k-firmware/package.mk
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="ath10k-firmware"
+PKG_VERSION="1ac853f"
+PKG_SHA256=""
+PKG_ARCH="any"
+PKG_LICENSE="other"
+PKG_SITE="https://github.com/chewitt/ath10k-firmware"
+PKG_URL="https://github.com/chewitt/ath10k-firmware/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="linux-firmware"
+PKG_SHORTDESC="ath10k-firmware: ath10k firmware from linux-firmware"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  FW_TARGET_DIR=$INSTALL/$(get_full_firmware_dir)
+
+  if find_file_path firmwares/$PKG_NAME.dat; then
+    FW_LISTS="${FOUND_PATH}"
+  else
+    FW_LISTS="${PKG_DIR}/firmwares/any.dat ${PKG_DIR}/firmwares/${TARGET_ARCH}.dat"
+  fi
+
+  for fwlist in ${FW_LISTS}; do
+    [ -f ${fwlist} ] || continue
+    while read -r fwline; do
+      [ -z "${fwline}" ] && continue
+      [[ ${fwline} =~ ^#.* ]] && continue
+      [[ ${fwline} =~ ^[[:space:]] ]] && continue
+
+      for fwfile in $(cd ${PKG_BUILD} && eval "find ${fwline}"); do
+        [ -d ${PKG_BUILD}/${fwfile} ] && continue
+        if [ -f ${PKG_BUILD}/${fwfile} ]; then
+          mkdir -p $(dirname ${FW_TARGET_DIR}/${fwfile})
+            cp -Lv ${PKG_BUILD}/${fwfile} ${FW_TARGET_DIR}/${fwfile}
+        else
+          echo "ERROR: Firmware file ${fwfile} does not exist - aborting"
+          exit 1
+        fi
+      done
+    done < ${fwlist}
+  done
+}
