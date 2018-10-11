d0f8b56b0551f827db90a5a3c25740e009e40b2d
Oleg Ivanov <balbes-150@yandex.ru>
2018-10-11 10:40:53 +0300

    fix kvim2

--- projects/Amlogic/devices/Khadas_VIM2/bootloader/update.sh
+++ projects/Amlogic/devices/Khadas_VIM2/bootloader/update.sh
@@ -1,66 +1,33 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
+[ -z "$UPDATE_DIR" ] && UPDATE_DIR="/storage/.update"
 [ -z "$SYSTEM_ROOT" ] && SYSTEM_ROOT=""
 [ -z "$BOOT_ROOT" ] && BOOT_ROOT="/flash"
 [ -z "$BOOT_PART" ] && BOOT_PART=$(df "$BOOT_ROOT" | tail -1 | awk {' print $1 '})
-
-# identify the boot device
-  if [ -z "$BOOT_DISK" ]; then
-    case $BOOT_PART in
-      /dev/sd[a-z][0-9]*)
-        BOOT_DISK=$(echo $BOOT_PART | sed -e "s,[0-9]*,,g")
-        ;;
-      /dev/mmcblk*)
-        BOOT_DISK=$(echo $BOOT_PART | sed -e "s,p[0-9]*,,g")
-        ;;
-    esac
-  fi
+if [ -z "$BOOT_DISK" ]; then
+  case $BOOT_PART in
+    /dev/sd[a-z][0-9]*)
+      BOOT_DISK=$(echo $BOOT_PART | sed -e "s,[0-9]*,,g")
+      ;;
+    /dev/mmcblk*)
+      BOOT_DISK=$(echo $BOOT_PART | sed -e "s,p[0-9]*,,g")
+      ;;
+  esac
+fi
 
 # mount $BOOT_ROOT r/w
-  mount -o remount,rw $BOOT_ROOT
+mount -o remount,rw $BOOT_ROOT
 
 # update device tree
-  if [ -f $SYSTEM_ROOT/usr/share/bootloader/dtb.img ]; then
-    echo "Updating dtb.img"
-    cp -p $SYSTEM_ROOT/usr/share/bootloader/dtb.img $BOOT_ROOT
-  fi
+mv $BOOT_ROOT/dtb $BOOT_ROOT/dtb_old
+cp -R $UPDATE_DIR/.tmp/*/3rdparty/instboot/dtb $BOOT_ROOT
 
-# update aml_autoscript
-  if [ -f $SYSTEM_ROOT/usr/share/bootloader/aml_autoscript ]; then
-    echo "Updating aml_autoscript"
-    cp -p $SYSTEM_ROOT/usr/share/bootloader/aml_autoscript $BOOT_ROOT
-  fi
-
-# update emmc_autoscript
-  if [ -f $SYSTEM_ROOT/usr/share/bootloader/emmc_autoscript ]; then
-    echo "Updating emmc_autoscript"
-    cp -p $SYSTEM_ROOT/usr/share/bootloader/emmc_autoscript $BOOT_ROOT
-  fi
-
-# update s905_autoscript
-  if [ -f $SYSTEM_ROOT/usr/share/bootloader/s905_autoscript ]; then
-    echo "Updating s905_autoscript"
-    cp -p $SYSTEM_ROOT/usr/share/bootloader/s905_autoscript $BOOT_ROOT
-  fi
-
-# update aml_autoscript.zip
-  if [ -f $SYSTEM_ROOT/usr/share/bootloader/aml_autoscript.zip ]; then
-    echo "Updating aml_autoscript.zip"
-    cp -p $SYSTEM_ROOT/usr/share/bootloader/aml_autoscript.zip $BOOT_ROOT
-  fi
-
-# update uEnv.ini
-  if [ -f $SYSTEM_ROOT/usr/share/bootloader/uEnv.ini ]; then
-    echo "Updating uEnv.ini"
-    cp -p $SYSTEM_ROOT/usr/share/bootloader/uEnv.ini $BOOT_ROOT
-  fi
-
-# update emmc-uEnv.ini
-  if [ -f $SYSTEM_ROOT/usr/share/bootloader/emmc-uEnv.ini ]; then
-    echo "Updating emmc-uEnv.ini"
-    cp -p $SYSTEM_ROOT/usr/share/bootloader/emmc-uEnv.ini $BOOT_ROOT
-  fi
+if [ -f $SYSTEM_ROOT/usr/share/bootloader/u-boot -a ! -e /dev/system -a ! -e /dev/boot ]; then
+  echo "Updating u-boot on: $BOOT_DISK..."
+  dd if=$SYSTEM_ROOT/usr/share/bootloader/u-boot of=$BOOT_DISK conv=fsync bs=1 count=112 status=none
+  dd if=$SYSTEM_ROOT/usr/share/bootloader/u-boot of=$BOOT_DISK conv=fsync bs=512 skip=1 seek=1 status=none
+fi
 
 # mount $BOOT_ROOT r/o
   sync
