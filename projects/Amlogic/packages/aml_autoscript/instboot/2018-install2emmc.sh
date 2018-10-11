1c34cca0940bd25c0c7b4d93446e5fdbf6388793
Oleg Ivanov <balbes-150@yandex.ru>
2018-10-11 10:02:47 +0300

    v20181011

--- projects/Amlogic/packages/aml_autoscript/instboot/2018-install2emmc.sh
+++ projects/Amlogic/packages/aml_autoscript/instboot/2018-install2emmc.sh
@@ -3,9 +3,20 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
+BACKUP_DATE=$(date +%Y%m%d%H%M%S)
 DEV_EMMC="/dev/mmcblk1"
 
-mount -o rw,remount /flash
+IMAGE_UBOOT="/flash/uboot.img"
+
+if [ -f $IMAGE_UBOOT ] ; then
+    mount -o rw,remount /flash
+    dd if="${DEV_EMMC}" of="/flash/bootloader_$BACKUP_DATE.img" bs=1M count=4 conv=fsync 2> /dev/null
+    dd if=$IMAGE_UBOOT of="${DEV_EMMC}" conv=fsync 2> /dev/null
+    rm -f $IMAGE_UBOOT
+else
+    echo  "Not found new bootloader..."
+    exit 1
+fi
 
 dd if="${DEV_EMMC}" of=/flash/u-boot-default.img bs=1M count=4
 
@@ -18,8 +29,8 @@ if grep -q "${DEV_EMMC}p2" /proc/mounts ; then
 fi
 
 parted -s "${DEV_EMMC}" mklabel msdos
-parted -s "${DEV_EMMC}" mkpart primary fat32 700M 1212M
-parted -s "${DEV_EMMC}" mkpart primary ext4 1213M 1245M
+parted -s "${DEV_EMMC}" mkpart primary fat32 4M 516M
+parted -s "${DEV_EMMC}" mkpart primary ext4 517M 549M
 
 dd if=/flash/u-boot-default.img of="${DEV_EMMC}" conv=fsync bs=1 count=442
 dd if=/flash/u-boot-default.img of="${DEV_EMMC}" conv=fsync bs=512 skip=1 seek=1
@@ -28,13 +39,10 @@ sync
 
 IMAGE_KERNEL="/flash/Image"
 IMAGE_SYSTEM="/flash/SYSTEM"
-SCRIPT_EMMC="/flash/emmc_autoscript"
-SCRIPT_ENV="/flash/uEnv.ini"
+SCRIPT_ENV="/flash/extlinux/extlinux.conf"
 IMAGE_DTB="/flash/dtb"
 
-if [ -f $IMAGE_KERNEL -a -f $IMAGE_SYSTEM -a -f $SCRIPT_EMMC -a -f $SCRIPT_ENV ] ; then
-
-    mount -o rw,remount /flash
+if [ -f $IMAGE_KERNEL -a -f $IMAGE_SYSTEM -f $SCRIPT_ENV ] ; then
 
     if grep -q "${DEV_EMMC}p1" /proc/mounts ; then
       umount -f "${DEV_EMMC}p1"
@@ -48,11 +56,11 @@ if [ -f $IMAGE_KERNEL -a -f $IMAGE_SYSTEM -a -f $SCRIPT_EMMC -a -f $SCRIPT_ENV ]
 
         cp $IMAGE_KERNEL /tmp/system && sync
         cp $IMAGE_SYSTEM /tmp/system && sync
-        cp $SCRIPT_EMMC /tmp/system && sync
-        cp $SCRIPT_ENV /tmp/system && sync
+        mkdir /tmp/system/extlinux
+        cp $SCRIPT_ENV /tmp/system/extlinux && sync
         sed -e "s/LIBREELEC/LE_EMMC/g" \
           -e "s/STORAGE/DATA_EMMC/g" \
-          -i "/tmp/system/uEnv.ini"
+          -i "/tmp/system/extlinux/extlinux.conf"
 
         cp -r $IMAGE_DTB /tmp/system && sync
         umount /tmp/system
